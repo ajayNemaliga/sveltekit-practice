c4a04feb09612ca730c9509050d6f70480034b22
Ajay <nemaligaajayreddy80@gmail.com>
2024-07-04 14:26:56 +0530

    added root directory path to vite.congig

--- sveltekit-practice/vite.config.ts
+++ sveltekit-practice/vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	root: '../sveltekit-practice/src/app.html',
 	plugins: [sveltekit()]
 });
